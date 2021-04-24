d273e279c897c2e1e9edcb51e14fd06e34c30dc9
arina-novikova <arina-novikova-00@mail.ru>
2021-04-24 15:58:56 +0300

    Сделано 3 инд задания

---
+++ scriptmy.js
@@ -0,0 +1,26 @@
+
+function Srav()
+{
+    d=document
+    var a = Number(d.form2.x1.value)//для поля ввода a
+    var b = Number(d.form2.x2.value)//для поля ввода b
+    var y2 = Math.max(a,b)//Выбор максимального значения
+    d.form2.y2.value=y2;
+}
+
+function Imya()
+{
+d=document
+var admin, name; // можно объявить две переменные через запятую
+name = "Арина";
+admin = name;
+alert( admin ); // "Арина"
+}
+
+function Old()
+{
+d-document
+var age = Number(d.form4.x1.value)
+age = old;
+alert( old );
+}
